91154463913cee6087eacbd10a65415425667365
Daniel <569755074@qq.com>
2019-01-24 14:41:56 +0800

    feat(Boost the process of compare): the speed is two times than before
    use stream() method to retrieve distinct elements in list that it can speed up the process. The JDK
    must be 10+, otherwise the programme will not run.

--- src/main/java/com/iobTool/ClearUnknown.java
+++ src/main/java/com/iobTool/ClearUnknown.java
@@ -1,17 +1,12 @@
 package com.iobTool;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.iobTool.util.FileNameUtil;
-import com.iobTool.util.InfoUtil;
 import com.iobTool.util.PublicTool;
 
-import javax.rmi.CORBA.Util;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * This tool is used to quickly clear tags in unknown file
@@ -33,30 +28,33 @@ public class ClearUnknown {
     public static void main(String[] args) throws IOException {
         System.out.println("准备开始...");
         ClearUnknown cu = new ClearUnknown();
-//        cu.addWord();
         cu.clearKnown();
+        cu.addWord();
     }
 
-    public  ClearUnknown() {
-        knownKeepList = new ArrayList<String>();
-        tagString = new ArrayList<String>();
-        unknownKeepList = new ArrayList<String>();
+    public ClearUnknown() {
+        knownKeepList = new ArrayList<>();
+        tagString = new ArrayList<>();
+        unknownKeepList = new ArrayList<>();
         unknownFilePath = FileNameUtil.ROOT_PATH + FileNameUtil.UNKNOWN;
         knownFilePath = FileNameUtil.ROOT_PATH + FileNameUtil.KNOWN;
         dicFilePath = FileNameUtil.ROOT_PATH + FileNameUtil.DIC;
         testFilePath = FileNameUtil.ROOT_PATH + FileNameUtil.TEST;
     }
 
-    public void compareWord() throws IOException {
+    private void compareWord() throws IOException {
+        System.out.println("去重前list的大小" + tagString.size());
+        var newArray = tagString.stream().distinct().toArray();
+        System.out.println("去重后list的大小" + newArray.length);
         FileReader fileReader = new FileReader(unknownFilePath);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         String line;
-        int i = 1;
+        var i = 1;
         System.out.println("准备完毕，开始处理Unknown文件...");
-        while ((line = bufferedReader.readLine()) != null){
-            Boolean isContain = false;
-            for (String word : tagString){
-                if (line.contains(word)) {
+        while ((line = bufferedReader.readLine()) != null) {
+            var isContain = false;
+            for (int j = 0; j < newArray.length; j++) {
+                if (line.contains(newArray[j].toString())) {
                     isContain = true;
                     break;
                 }
@@ -65,9 +63,9 @@ public class ClearUnknown {
                 unknownKeepList.add(line);
 
             i++;
-            if(i%50 == 0)
+            if (i % 50 == 0)
                 System.out.print(".");
-            if(i%500 == 0) {
+            if (i % 500 == 0) {
                 System.out.println();
                 System.out.println("正在处理Unknown文件，已完成：" + i * 100 / 278315 + "%");
             }
@@ -80,23 +78,23 @@ public class ClearUnknown {
         FileReader fileReader = new FileReader(dicFilePath);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         String line;
-        while ((line = bufferedReader.readLine()) != null){
-            JSONObject json = JSON.parseObject(line);
-            String text = json.getString("text");
-            JSONArray label = json.getJSONArray("label");
-            int startIndex = 0;
-            int endIndex = 0;
+        while ((line = bufferedReader.readLine()) != null) {
+            var json = JSON.parseObject(line);
+            var text = json.getString("text");
+            var label = json.getJSONArray("label");
+            var startIndex = 0;
+            var endIndex = 0;
             while (startIndex < label.size()) {
                 //只有一个字
                 if (label.size() == 1) {
 //                    if (!tagString.contains(text.substring(0, 1))) {
 //                        System.out.println(text.substring(0, 1));
-                        tagString.add(text.substring(0, 1));
+                    tagString.add(text.substring(0, 1));
                     break;
                 }
 
                 //找到词语在一句话中开始的位置
-                for (int i = startIndex; i < label.size(); i++) {
+                for (var i = startIndex; i < label.size(); i++) {
                     if (label.get(i).toString().contains("B-")) {
                         startIndex = i;
                         break;
@@ -105,23 +103,23 @@ public class ClearUnknown {
                         startIndex = i;
                 }
                 //找到词语在一句话中结束的位置
-                for (int j = startIndex + 1; j < label.size(); j++) {
+                for (var j = startIndex + 1; j < label.size(); j++) {
                     if (!label.get(j).toString().contains("I-")) {
                         endIndex = j;
                         break;
                     }
-                    if (j >= label.size() - 1){
+                    if (j >= label.size() - 1) {
                         endIndex = label.size();
                         break;
                     }
                 }
 
                 //如果最后一位为单独的一个字
-                if (startIndex == label.size() - 1){
-                    if(label.get(label.size() - 1).toString().contains("B-")){
+                if (startIndex == label.size() - 1) {
+                    if (label.get(label.size() - 1).toString().contains("B-")) {
 //                        if (!tagString.contains(text.substring(label.size() - 1, label.size()))) {
 //                            System.out.println(text.substring(label.size() - 1, label.size()));
-                            tagString.add(text.substring(label.size() - 1, label.size()));
+                        tagString.add(text.substring(label.size() - 1, label.size()));
 
                     }
                     break;
@@ -130,7 +128,7 @@ public class ClearUnknown {
                 //新词加入list
 //                if (!tagString.contains(text.substring(startIndex, endIndex))) {
 //                    System.out.println(text.substring(startIndex, endIndex));
-                    tagString.add(text.substring(startIndex, endIndex));
+                tagString.add(text.substring(startIndex, endIndex));
 
                 startIndex = endIndex + 1;
             }
@@ -148,13 +146,13 @@ public class ClearUnknown {
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         String line;
         while ((line = bufferedReader.readLine()) != null) {
-            Boolean delete = true;
-            JSONObject json = JSON.parseObject(line);
-            JSONArray label = json.getJSONArray("label");
+            var delete = true;
+            var json = JSON.parseObject(line);
+            var label = json.getJSONArray("label");
             for (Object tag : label)
                 if (!tag.toString().equals("O"))
                     delete = false;
-            if(!delete)
+            if (!delete)
                 knownKeepList.add(line);
         }
         PublicTool.writeLines(testFilePath, knownKeepList);
